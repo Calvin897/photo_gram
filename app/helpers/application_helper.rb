792c92b0bb6f840b5b365d0a982667f7baa190c8
calvin tribelhorn <calvintribby@gmail.com>
2021-06-02 11:34:17 +0200

    added flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+    def alert_for(flash_type)
+        { success: 'alert-success alert-dismissible fade show',
+          error: 'alert-danger alert-dismissible fade show',
+          alert: 'alert-warning alert-dismissible fade show',
+          notice: 'alert-info alert-dismissible fade show'
+        }[flash_type.to_sym] || flash_type.to_s
+      end
 end

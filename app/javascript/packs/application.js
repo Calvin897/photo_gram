dfc191e3a00f88e347370163bdd1868b3705d7af
calvin tribelhorn <calvintribby@gmail.com>
2021-06-04 22:39:37 +0200

    add js function to change new image onChange event

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,9 +13,7 @@ Rails.start();
 Turbolinks.start();
 ActiveStorage.start();
 
-var loadFile = function(event) {
+window.loadFile = function(event) {
   var output = document.getElementById("image-preview");
   output.src = URL.createObjectURL(event.target.files[0]);
-  console.log(output.src);
-  debugger;
 };
